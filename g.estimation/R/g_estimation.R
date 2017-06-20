dc0eefad770f4559bb470487a8741b7d05c78f6f
RyanGan <ryan.william.gan@gmail.com>
2017-06-20 12:05:25 -0600

    attempting to make a package

---
+++ g.estimation/R/g_estimation.R
@@ -0,0 +1,38 @@
+#' G-estimation fuction
+#' 
+#' This function calculates a "causal" estimate of abinary treatment/exposure (a)
+#' on an outcome (y). It can calculate the risk difference or risk ratio.
+#' @param estimate Estimate difference or ratio. Defaults to "diff"
+#' @keywords gestimation
+#' @export
+#' @examples 
+#' g.estimation()
+
+# g-estimation function
+g.estimation <- function(data, y, a, q.model, model.family, estimate="diff"){
+  # create a = 1 and a = 0 dataframes ----
+  a1_df <- a0_df <- data
+  a1_df[, a] <- 1
+  a0_df[, a] <- 0
+  # run q-formula ----
+    model <- glm(as.formula(paste(y, q_mod, sep = "~")), data = data, 
+                 family = mod_family)
+  # calculate mean expected in a1 and a0
+    pr_a1 <- mean(predict(model, newdata = a1_df, type = "response")) 
+    pr_a0 <- mean(predict(model, newdata = a0_df, type = "response"))
+
+  # calculate desired estimates "diff" or "ratio" ----
+    # calculate difference a1 to a0
+    if(estimate == "diff"){
+      msm_diff <- pr_a1 - pr_a0
+      msm_val <- msm_diff
+    }
+    # calculate ratio of a1/a0
+    if(estimate == "ratio"){
+      msm_ratio <- pr_a1/pr_a0 
+      msm_val <- msm_ratio
+    } 
+    # output 
+      return(msm_val)
+      
+} # end function
